2483a99acb85ea280bb6cb5e1a5e9cbd3999c6d4
Gwanaelle Davesne <145539127+GwanaelleDavesne@users.noreply.github.com>
2024-12-02 12:03:59 +0100

    Ajout de RocketPokemonFactory

--- TP-techniques-de-test/src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ TP-techniques-de-test/src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -17,7 +17,7 @@ public class IPokemonFactoryTest {
 	@BeforeEach
 	void setup() {
 		pokemonMetadataProvider = new PokemonMetadataProvider();
-		pokemonFactory = new PokemonFactory(pokemonMetadataProvider);
+		pokemonFactory = new RocketPokemonFactory();
 	}
 	
 	@Test
@@ -27,22 +27,28 @@ public class IPokemonFactoryTest {
 		
 		assertEquals(0,bulbizarre.getIndex());
 		assertEquals("Bulbizarre",bulbizarre.getName());
-		assertEquals(8,bulbizarre.getAttack());
-		assertEquals(8,bulbizarre.getDefense());
-		assertEquals(9,bulbizarre.getStamina());
 		assertEquals(613,bulbizarre.getCp());
 		assertEquals(64,bulbizarre.getHp());
 		assertEquals(4000,bulbizarre.getDust());
 		assertEquals(4,bulbizarre.getCandy());
-		assertEquals(55,bulbizarre.getIv());
+		assertEquals(1,bulbizarre.getIv());
 	}
 	
 	@Test
 	void createPokemonInvalidParameterTest(){
-		//test pour vérifier la non création d'un Pokemon  en cas de paramètres invalides
-		assertThrows(IllegalArgumentException.class, () -> {
-			pokemonFactory.createPokemon(-1, -613, -64, -4000, -4);
-	    });
+		//test pour vérifier la création d'un Pokemon  en cas de paramètres invalides
+		Pokemon pokemon = pokemonFactory.createPokemon(-1,613,64,4000,4);
+		
+		assertEquals(-1,pokemon.getIndex());
+		assertEquals("Ash's Pikachu",pokemon.getName());
+		assertEquals(1000,pokemon.getAttack());
+		assertEquals(1000,pokemon.getDefense());
+		assertEquals(1000,pokemon.getStamina());
+		assertEquals(613,pokemon.getCp());
+		assertEquals(64,pokemon.getHp());
+		assertEquals(4000,pokemon.getDust());
+		assertEquals(4,pokemon.getCandy());
+		assertEquals(0,pokemon.getIv());
 	}
 
 }
